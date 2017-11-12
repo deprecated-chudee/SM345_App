b523d81bdb901ce39f854ea3f80b7e9c37b68cef
chudee <geonwoo.chu@gmail.com>
2017-11-12 23:04:46 +0900

    server.service.ts && login.ts 코드 정리

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, URLSearchParams, RequestOptions} from '@angular/http';
+import { Http, Headers, URLSearchParams, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
-import {Observable} from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 import { Message } from '../models/message';
 import { Article } from '../models/article';
 import { User } from '../models/user';
@@ -10,65 +10,62 @@ import { Mentoroom } from '../models/mentoroom';
 @Injectable()
 export class ServerService {
   
-  //private URL = 'http://localhost:8085/sm345/api/';
-  private URL = 'http://220.230.112.31:8081/sm345/api'
+    //private TESTURL = 'http://localhost:8085/sm345/api/';
+    private URL = 'http://220.230.112.31:8081/sm345/api/';
 
-  private http: Http;
-  static USERID: number;
-  static USERAUTH: number;
-  static USERNAME: string;
+    static USERID: number;
+    static USERAUTH: number;
+    static USERNAME: string;
 
-  constructor(http: Http) { 
-    this.http = http;
-  }
+    constructor(private http: Http) { 
+        console.log(ServerService.USERID)
+        console.log(ServerService.USERAUTH)
+        console.log(ServerService.USERNAME)
+    }
 
-  makeLogin(user: User): Promise<Message> {
-    let url = this.URL + 'login';
-    return this.http.post(url, user)
-              .toPromise()
-              .then(response => 
-                response.json() as Message)
-              .catch(this.handleError);
-  }
+    makeLogin(user: User): Promise<Message> {
+        let url = this.URL + 'login';
+        return this.http.post(url, user)
+            .toPromise()
+            .then(response => response.json() as Message)
+            .catch(this.handleError);
+    }
 
-  getLoginrecord(user_id: string): Promise<Message> {
-    let url = this.URL + 'login_record/' + user_id;
-    return this.http.get(url)
-              .toPromise()
-              .then(response => 
-                response.json() as Message)
-              .catch(this.handleError);
-  }
+    getLoginrecord(user_id: string): Promise<Message> {
+        let url = this.URL + 'login_record/' + user_id;
+        return this.http.get(url)
+            .toPromise()
+            .then(response => response.json() as Message)
+            .catch(this.handleError);
+    }
 
-  updatePassword(user: User): Promise<Message>{
-    let url = this.URL + 'updatepassword';
-    return this.http.post(url, user)
-              .toPromise()
-              .then(response => 
-                response.json() as Message)
-              .catch(this.handleError);
-  }
+    updatePassword(user: User): Promise<Message>{
+        let url = this.URL + 'updatepassword';
+        return this.http.post(url, user)
+            .toPromise()
+            .then(response => response.json() as Message)
+            .catch(this.handleError);
+    }
 
-  createMentoroom(mentoroom: Mentoroom): Promise<Message>{
-    let url = this.URL + 'mentoroom/create';
-    return this.http.post(url, mentoroom)
-              .toPromise()
-              .then(response => 
-                response.json() as Message)
-              .catch(this.handleError);
-  }
+    createMentoroom(mentoroom: Mentoroom): Promise<Message>{
+        let url = this.URL + 'mentoroom/create';
+        return this.http.post(url, mentoroom)
+            .toPromise()
+            .then(response => response.json() as Message)
+            .catch(this.handleError);
+    }
 
-  getMentoroomList(): Promise<Mentoroom[]> {
-    let url = this.URL + 'mentoroom'
-    return this.http.get(url)
-              .toPromise()
-              .then(response => response.json() as Mentoroom[])
-              .catch(this.handleError);
-  }
+    getMentoroomList(): Promise<Mentoroom[]> {
+        let url = this.URL + 'mentoroom'
+        return this.http.get(url)
+            .toPromise()
+            .then(response => response.json() as Mentoroom[])
+            .catch(this.handleError);
+    }
 
 
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
-  }
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,148 +1,136 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController, AlertController, ToastController, App } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { 
+    NavController, 
+    ModalController, 
+    AlertController, 
+    ToastController, 
+    App } from 'ionic-angular';
+
 import { HomePage } from '.././home/home';
 import { SmLoginPage } from '../smLogin/smLogin';
-import { ServerService } from '../../app/server.service';
-import { Http } from '@angular/http';
 import { MyApp } from '../../app/app.component';
 import { User } from '../../models/user';
 import { Message } from '../../models/message';
-//import { CoolLocalStorage } from 'angular2-cool-storage';
+
+import { ServerService } from '../../app/server.service';
 
 @Component({
-  templateUrl: 'login.html'
+    templateUrl: 'login.html'
 })
 export class LoginPage {
+    private user: User = new User("","",0,"");
+    private message: Message = new Message;
+    private login_record: number =0;
+    private password: string = "";
 
-  private user: User;
-  private http: Http;
-  serverService: ServerService;
-  private message: Message;
-  //localStorage: CoolLocalStorage;
-  private login_record: number =0;
-  private password: string = "";
+    constructor(
+        public app: App, 
+        private serverService: ServerService, 
+        private http: Http, 
+        public navCtrl: NavController, 
+        public appCtrl: App, 
+        public toastCtrl: ToastController, 
+        public alertCtrl: AlertController, 
+        public modalCtrl: ModalController
+    ) { }
 
-  constructor(public app: App, serverService: ServerService, http: Http, public navCtrl: NavController, public appCtrl: App, public toastCtrl: ToastController, public alertCtrl: AlertController, public modalCtrl: ModalController, /*localStorage: CoolLocalStorage*/) {
-    this.http = http;
-    this.serverService = serverService;          
-    this.user = new User("","",0,"");
-    this.navCtrl = navCtrl;
-    this.message = new Message;
-    //this.localStorage = localStorage;   
-  }
-
-  //로그인
-  signIn(){
-    this.serverService.getLoginrecord(this.user.user_id)
-    .then(message =>
-    {
-      this.login_record = message.login_record;
-    });
-
-    if(this.login_record == 0)
-      this.showPasswordAlert();
-    else{
-    this.serverService.makeLogin(this.user)
-    .then(message =>
-      { 
-        if(message.key == -1)
-          this.presentLoginToast(message);
-        if(message.key == 2)
-          this.presentLoginToast(message);
-        if(message.key == 0){
-        
-         
-          //this.localStorage.setItem('USERID', message.user_id.toString());
-          //this.localStorage.setItem('USERAUTH', message.user_auth.toString());
-          //this.localStorage.setItem('USERNAME', message.user_name);
-         
-          ServerService.USERID = message.user_id;
-          ServerService.USERNAME = message.user_name;
-          ServerService.USERAUTH = message.user_auth;
-
-          this.presentLoginToast(message);
-          
-          this.appCtrl.getRootNav().setRoot(MyApp);
-          window.location.reload();
-         
-        }
-      });
-    }
-    }
-
-  presentLoginToast(message) {
-      let toast = this.toastCtrl.create({
-      message: message.title,
-      duration: 3000,
-      position: 'bottom',
-      });
-      toast.present();
-  }
-
-  openHomePage() {
-    this.navCtrl.setRoot(HomePage);
-  }
-
-  openSmLoginPage() {
-    let modal = this.modalCtrl.create(SmLoginPage);
-    modal.present();
-  }
-
-  showPasswordAlert() {
-    let prompt = this.alertCtrl.create({
-       title: '비밀번호 변경',
-       inputs: [
-            {
-              name: 'password',
-              type: 'password',
-              placeholder: '기존 비밀번호',
-           },
-           {
-              name: 'newPassword',
-              type: 'password', 
-              placeholder: '새 비밀번호',
-           },
-           {
-              name: 'newPasswordCheck',
-              type: 'password', 
-              placeholder: '비밀번호 확인',
-          }
-       ],
-       buttons: [
-          {
-             text: '취소',
-             handler: data => {
-             }
-          },
-          {
-             text: '확인',
-             handler: data => {
-              //비번이 기존꺼랑 일치하는지 여부 틀리면 틀리다.
-              
-              this.user.user_password = data['newPassword'];
-              this.serverService.updatePassword(this.user)
-              .then(message =>
-              {
-                this.presentLoginToast(message);
-
-                //this.localStorage.setItem('USERID', message.user_id.toString());
-               // this.localStorage.setItem('USERAUTH', message.user_auth.toString());
-                //this.localStorage.setItem('USERNAME', message.user_name);
-
-                ServerService.USERID = message.user_id;
-                ServerService.USERNAME = message.user_name;
-                ServerService.USERAUTH = message.user_auth;
-              
-                this.appCtrl.getRootNav().setRoot(MyApp);
-                window.location.reload();
-               
+    //로그인
+    signIn() {
+        this.serverService.getLoginrecord(this.user.user_id)
+            .then(message => {
+                this.login_record = message.login_record;
             });
-            }                   
-          }
-       ]
-    });
-    prompt.present();
+
+        if(this.login_record == 0) this.showPasswordAlert();
+        else {
+            this.serverService.makeLogin(this.user)
+                .then(message => { 
+                    if(message.key === -1)
+                       this.presentLoginToast(message);
+                    if(message.key === 2)
+                       this.presentLoginToast(message);
+                    if(message.key === 0) {
+                        ServerService.USERID = message.user_id;
+                        ServerService.USERNAME = message.user_name;
+                        ServerService.USERAUTH = message.user_auth;
+
+                        this.presentLoginToast(message);
+          
+                        this.appCtrl.getRootNav().setRoot(MyApp);
+                        window.location.reload();
+                    }
+              });
+        }
     }
-  }
+
+    presentLoginToast(message) {
+        let toast = this.toastCtrl.create({
+            message: message.title,
+            duration: 3000,
+            position: 'bottom',
+        });
+        toast.present();
+    }
+
+    openHomePage() {
+        this.navCtrl.setRoot(HomePage);
+    }
+
+    openSmLoginPage() {
+        let modal = this.modalCtrl.create(SmLoginPage);
+        modal.present();
+    }
+
+    showPasswordAlert() {
+        let prompt = this.alertCtrl.create({
+            title: '비밀번호 변경',
+            inputs: [
+                {
+                    name: 'password',
+                    type: 'password',
+                    placeholder: '기존 비밀번호',
+                },
+                {
+                    name: 'newPassword',
+                    type: 'password', 
+                    placeholder: '새 비밀번호',
+                },
+                {
+                    name: 'newPasswordCheck',
+                    type: 'password', 
+                    placeholder: '비밀번호 확인',
+                }
+            ],
+            buttons: [
+                {
+                    text: '취소',
+                    handler: data => { }
+                },
+                {
+                    text: '확인',
+                    handler: data => {
+                        //비번이 기존꺼랑 일치하는지 여부 틀리면 틀리다.
+                        
+                        this.user.user_password = data['newPassword'];
+                        this.serverService.updatePassword(this.user)
+                        .then(message => {
+                            this.presentLoginToast(message);
+
+                            ServerService.USERID = message.user_id;
+                            ServerService.USERNAME = message.user_name;
+                            ServerService.USERAUTH = message.user_auth;
+                          
+                            this.appCtrl.getRootNav().setRoot(MyApp);
+                            window.location.reload();
+                        });
+                    }                   
+                }
+            ]
+        });
+
+        prompt.present();
+    }
+}
 
 
