d3a454bbdab6f732956df7529f48dc095f3c6d2e
chudee <geonwoo.chu@gmail.com>
2017-12-14 20:12:03 +0900

    멘토, 멘티, 설문조사 날짜 체크 구현

--- src/pages/room/room.ts
+++ src/pages/room/room.ts
@@ -5,43 +5,60 @@ import { RoomDetailPage } from '.././roomDetail/roomDetail';
 import { MentorAddPage } from '.././mentorAdd/mentorAdd';
 
 import { MentoroomService } from '../../services/mentoroom.service';
+import { AdminService } from '../../services/admin.service';
 
 import { Mentoroom } from '../../models/mentoroom';
 
 @Component({
-  templateUrl: 'room.html'
+    templateUrl: 'room.html'
 })
 export class RoomPage  implements OnInit {
-  private mentorooms: Mentoroom[];
-  private currentUser;
-  private USERAUTH;
+    private mentorooms: Mentoroom[];
+    private currentUser;
+    private USERAUTH;
+    private mento_start;
+    private mento_end;
+    private date: Date;
 
-  constructor( 
-    private mentoroomService: MentoroomService, 
-    public navCtrl: NavController, 
-    public modalCtrl: ModalController
-  ) {
-    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    this.USERAUTH = this.currentUser.USERAUTH;
-  }
+    constructor( 
+        private mentoroomService: MentoroomService, 
+        private adminService: AdminService,
+        public navCtrl: NavController, 
+        public modalCtrl: ModalController
+    ) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        this.USERAUTH = this.currentUser.USERAUTH;
+        this.date = new Date();
+    }
 
-  ngOnInit() {
-    this.mentoroomService.getMentoroomList()
-    .then(mentorooms => this.mentorooms = mentorooms);
-  }
+    ngOnInit() {
+        this.mentoroomService.getMentoroomList()
+            .then(mentorooms => this.mentorooms = mentorooms);
+        this.getMentoRoomInfo()
+    }
 
-  openHomePage() {
-    this.navCtrl.setRoot(HomePage);
-  }
+    openHomePage() {
+        this.navCtrl.setRoot(HomePage);
+    }
 
-  openRoomDetailPage(selectedRoom) {
-    this.navCtrl.push(RoomDetailPage, {
-      selectedRoom: selectedRoom
-    });
-  }
+    openRoomDetailPage(selectedRoom) {
+        this.navCtrl.push(RoomDetailPage, {
+            selectedRoom: selectedRoom
+        });
+    }
 
-  openMentorAddPage() {
-    let modal = this.modalCtrl.create(MentorAddPage);
-    modal.present();
-  }
+    openMentorAddPage() {
+        let modal = this.modalCtrl.create(MentorAddPage);
+        modal.present();
+    }
+
+    getMentoRoomInfo() {
+        this.adminService.getMentoRoomInfo()
+            .then(res => {
+                this.mento_start = new Date(res.mento_start);
+                this.mento_end = new Date(res.mento_end);
+        })
+    }
+
+
 }
